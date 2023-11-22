eebdb21be844b79ceaf9ff93d4ab22542ef52dcb
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-11-22 11:09:02 +0100

    JS: add function added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+const add(prevResult, enteredNumber) {
+    const prevResult = currentResult
+    const enteredNumber = +userInput.value;
+    
+    const result = prevResult + enteredNumber;
+}
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
