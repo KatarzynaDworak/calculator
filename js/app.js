5c7f9ec7d2ed9820ffec6c7902c8945a2a49618b
KatarzynaDworak <k.dw@poczta.onet.pl>
2023-11-22 16:21:33 +0100

    JS calculationDescription function added

--- js/app.js
+++ js/app.js
@@ -5,11 +5,15 @@ function getUserNumberInput() {
    return +userInput.value;
 }
 
+function calculationDescription(oparator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `(${resultBeforeCalc} ${operator} ${calcNumber}`;
+}
+
 function add() {
     const enteredNumber = getUserNumberInput;
-    const calcDescription = `(${currentResult} ${operator} ${enteredNumber}`;
     currentResult += enteredNumber;
     outputResult(currentResult, calcDescription);
+    calculationDescription("+", currentResult, enteredNumber);
 }
 
 function subtract() {
